3e239cccde52a95d290d471756f5b5e1e26f9c07
JustynaBroniszewska <jusbroniszewska@gmail.com>
2018-07-03 18:13:44 +0200

    First day of Solidity Challenge.

---
+++ Day1_Greeter/Greeter.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.22;
+
+contract Greeter {
+    string greeting;
+    address owner;
+    
+    constructor(string _greeting) public{
+        greeting = _greeting;
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwner(){
+        require(msg.sender == owner, "You aren't contract owner. ");
+        _;
+    }
+    
+    function sayHello() public view returns(string){
+        if(msg.sender == owner) {
+            return "Hello Daddy";
+        }
+        else{
+            return greeting;
+        }
+    }
+    function setGreeting(string _newGreeting) public onlyOwner{
+        greeting = _newGreeting;
+        
+    }
+}
