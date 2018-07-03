2a3d302cade9780feec28df2468213f37c627149
JustynaBroniszewska <jusbroniszewska@gmail.com>
2018-07-03 19:57:44 +0200

    Second Day: Balance Address Checker

---
+++ Day2_BalanceAddressChecker/BalanceAddressChecker.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.22;
+contract BalanceAddressChecker{
+    address owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+    function getContractAddress() public view returns(address){
+        return this;
+    }
+    function getContractBalance() public view returns(uint){
+        return this.balance;
+    }
+    function getSenderAddress() public view returns(address) {
+        return msg.sender;
+    }
+    function getOwnerAddress() public view returns(address) {
+        return owner;
+    }
+    modifier onlyOwner(){
+        require(msg.sender == owner, "You aren't contract owner.");
+        _;
+    }
+    function getSenderBalance() public view returns(uint){
+        return msg.sender.balance;
+    }
+    function getOwnerBalance() public view onlyOwner returns(uint) {
+        return owner.balance;
+    }
+}
